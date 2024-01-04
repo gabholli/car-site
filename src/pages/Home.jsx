b766d500e18856e657fc5e4c375a0b1e50ee8930
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-01-03 23:00:05 -0800

    Changed text in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom"
 export default function Home() {
     return (
         <div className="flex flex-col justify-center items-center gap-8 px-2">
-            <h1 className="text-center text-4xl">Find out about your favorite car</h1>
-            <Link className="bg-gray-500 px-4 py-2 rounded text-xl" to="companies">Find your car</Link>
+            <h1 className="text-center text-4xl">Find information about car manufacturer</h1>
+            <Link className="bg-gray-500 px-4 py-2 rounded text-xl" to="companies">Find your manufacturer</Link>
         </div>
     )
 }
