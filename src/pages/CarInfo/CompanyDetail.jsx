85bfaa792181a11c096c42dc82de9fa37658cda1
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-01-04 15:11:29 -0800

    Altered two components
    Add API call to Company Detail and used outlet context with company detail and info components.

--- src/pages/CarInfo/CompanyDetail.jsx
+++ src/pages/CarInfo/CompanyDetail.jsx
@@ -4,6 +4,7 @@ import { NavLink, useParams, Outlet } from "react-router-dom"
 
 export default function CompanyDetail() {
     const { id } = useParams()
+    const [companyData, setCompanyData] = useState([])
 
     useEffect(() => {
         fetch(`https://vpic.nhtsa.dot.gov/api/vehicles/getmanufacturerdetails/${id}?format=json`)
@@ -15,11 +16,22 @@ export default function CompanyDetail() {
             })
             .then(data => {
                 console.log(data)
+                setCompanyData(data.Results)
             })
             .catch(error =>
                 console.log("Fetch error: ", error))
-    }, [])
+    }, [id])
 
+    // const manufacturerData = companyData?.map(item => {
+    //     return (
+    //         <div>
+    //             <h1>{titleCase("address")}</h1>
+    //             <h1>City</h1>
+    //             <h1>Country: </h1>
+    //         </div>
+    //     )
+    // })
+    console.log(companyData)
 
     return (
         <div className="flex flex-col items-center p-4">
@@ -31,7 +43,7 @@ export default function CompanyDetail() {
                     Details
                 </NavLink>
             </nav>
-            <Outlet />
+            <Outlet context={{ companyData }} />
         </div>
     )
 }
--- src/pages/CarInfo/CompanyInfo.jsx
+++ src/pages/CarInfo/CompanyInfo.jsx
@@ -1,9 +1,33 @@
 import React from "react"
+import { useEffect } from "react"
+import { useOutletContext } from "react-router-dom"
 
 export default function CompanyDetail() {
+
+    const { companyData } = useOutletContext()
+
+    // function titleCase(word) {
+    //     return word.charAt(0).toUpperCase() + word.substr(1).toLowerCase()
+    // }
+
+    const manufacturerData = companyData?.map(item => {
+        return (
+            <div className="flex flex-col gap-4" key={item.Mfr_ID}>
+                <h1 className="font-bold">Address: </h1>
+                <p>{item.Address}</p>
+                <h1 className="font-bold">City: </h1>
+                <p>{item.City}</p>
+                <h1 className="font-bold">Country: </h1>
+                <p>{item.Country}</p>
+            </div>
+        )
+    })
+
+
+
     return (
-        <div className="m-auto">
-            <h1>Company details</h1>
-        </div>
+        <section className="m-auto">
+            {manufacturerData}
+        </section>
     )
 }
