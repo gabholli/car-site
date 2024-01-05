0c41c08fa4b8b876caf5d768d57e56867a391db5
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-01-05 00:31:29 -0800

    Added underline on hover for home page link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,11 @@ export default function Home() {
     return (
         <div className="flex flex-col justify-center items-center gap-8 px-2">
             <h1 className="text-center text-4xl">Find information about car manufacturers</h1>
-            <Link className="bg-orange-200 px-4 py-2 rounded text-xl" to="companies">Find your manufacturer</Link>
+            <Link
+                className="bg-orange-200 px-4 py-2 rounded text-xl hover:underline"
+                to="companies">
+                Find your manufacturer
+            </Link>
         </div>
     )
 }
