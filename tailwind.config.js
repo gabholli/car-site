a56a4ce7be0234e80e77959e3819b05993101cfd
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-01-05 17:10:06 -0800

    Added image to assets and updated Tailiwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'background-image': "url('./assets/highway.jpg')",
+      },
+    }
+    ,
   },
   plugins: [],
 }
