f322f06ca57fb1387db7baf702589cbaa61af210
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-01-06 16:00:51 -0800

    Sorted company locations by country name

--- src/pages/CarInfo/CompanyInfo.jsx
+++ src/pages/CarInfo/CompanyInfo.jsx
@@ -10,19 +10,26 @@ export default function CompanyDetail() {
     //     return word.charAt(0).toUpperCase() + word.substr(1).toLowerCase()
     // }
 
-    const manufacturerData = companyData?.map((item, index) => {
-        return (
-            <div className="flex flex-col w-48" key={item.Mfr_ID}>
-                <h1 className="text-center font-semibold text-lg underline mb-4">Location {index + 1}:</h1>
-                {item.Address && <h2 className="font-bold text-lg">Address: </h2>}
-                <p className="">{item.Address}</p>
-                {item.City && <h2 className="font-bold text-lg">City: </h2>}
-                <p className="">{item.City}</p>
-                {item.Country && <h2 className="font-bold text-lg">Country: </h2>}
-                <p className="mb-4">{item.Country}</p>
-            </div>
-        )
-    })
+    const manufacturerData = companyData
+        .sort((a, b) => {
+            if (a.Country?.toLowerCase() < b.Country?.toLowerCase()) return -1
+            if (a.Country?.toLowerCase() > b.Country?.toLowerCase()) return 1
+            return 0
+        })
+        .map((item, index) => {
+            return (
+                <div className="flex flex-col w-48" key={item.Mfr_ID}>
+                    <h1 className="text-center font-semibold text-lg underline mb-4">Location {index + 1}:</h1>
+                    {item.Address && <h2 className="font-bold text-lg">Address: </h2>}
+                    <p className="">{item.Address}</p>
+                    {item.City && <h2 className="font-bold text-lg">City: </h2>}
+                    <p className="">{item.City}</p>
+                    {item.Country && <h2 className="font-bold text-lg">Country: </h2>}
+                    <p className="mb-4">{item.Country}</p>
+                </div>
+            )
+        })
+
 
     const locationName = companyData?.map(item => item.Mfr_CommonName)
 
